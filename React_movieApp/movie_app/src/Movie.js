73146622f13f404643982e289d546e69a545a38d
Jay Yoon <jay.yoon7@gmail.com>
2019-10-03 21:46:12 +0200

    JS basic fundemental recap

---
+++ React_movieApp/movie_app/src/Movie.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Movie.css';
+
+class Movie extends Component{
+    render(){
+        return(
+            <div>
+                <h1>this is the movie</h1>
+            </div>
+
+        )
+    }
+} 
+
+export default Movie;
