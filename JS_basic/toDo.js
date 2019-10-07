3c0f147223398eb8317e0a48d3fa9cdabd221ada
Jay jiwon Yoon <jayjiwonyoon@Jays-MacBook-Pro.local>
2019-10-07 19:51:51 +0200

    toDo obj array added

--- JS_basic/toDo.js
+++ JS_basic/toDo.js
@@ -3,17 +3,24 @@ const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".js-toDoList")
 
 const TODOS_LS = "toDos"
+const toDos = [];
 
 function paintToDo(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     delBtn.value = "X";
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
     span.innerText = text;
     li.appendChild(span);
     li.appendChild(delBtn);
     toDoList.appendChild(li);
     toDoInput.value = "";
+    const toDoObj = {
+        text: text,
+        id: newId
+    }
+    toDos.push(toDoObj);
 }
 
 function handleSubmit(event) {
@@ -23,8 +30,8 @@ function handleSubmit(event) {
 }
 
 function loadTodos() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null){
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null){
 
     }
 }
