043f973203e6c3e5d1ffc5e0eb461736d758b1c8
Jay jiwon Yoon <jayjiwonyoon@Jays-MacBook-Pro.local>
2019-10-10 14:16:44 +0200

    getting useers geo langtitude and longitude and save it as an object

--- JS_basic/weather.js
+++ JS_basic/weather.js
@@ -1,7 +1,25 @@
 const COORDS = 'coords';
 
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj))
+}
+
+function handleGeoSucess(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    }
+    saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+    console.log("can not access geo location.");
+}
+
 function askForCoords() {
-    
+    navigator.geolocation.getCurrentPosition(handleGeoSucess, handleGeoError)
 }
 
 function loadCoords() {
