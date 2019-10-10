afdd6dd610c5dce054c465dffdd320eef1db05c6
Jay jiwon Yoon <jayjiwonyoon@Jays-MacBook-Pro.local>
2019-10-10 13:40:01 +0200

    load random bg image locally

--- JS_basic/bg.js
+++ JS_basic/bg.js
@@ -1,11 +1,21 @@
 const body = document.querySelector("body");
 
+const IMG_NUMBER = 3;
+
+function paintImage(imgNumber) {
+    const image = new Image();
+    image.src = `images/${imgNumber + 1}.jpg`
+    image.classList.add("bgImage");
+    body.prepend(image);
+}
+
 function getRandom(){
     const number = Math.floor(Math.random() * 3);
-    return number
+    return number;
 }
 
 function init() {
-    getRandom();
+    const randomNumber = getRandom();    
+    paintImage(randomNumber);
 }
 init();
